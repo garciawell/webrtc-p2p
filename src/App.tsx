f16bd68f3a2f70eb27cf94ae47ed00849a274457
Wellington Garcia <garciawell@gmail.com>
2024-11-06 17:40:42 -0300

    feat: update server stun

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,40 @@ const constraints = {
   video: true
 };
 
-
 const peer = new Peer({
   path: '/peerjs',
   host: 'concrete-kelly-garcia-2cd3d48b.koyeb.app',
   config: {
     'iceServers': [
       {
+        urls: "stun1.l.google.com:19302",
+      },
+      {
+        urls: "stun1.voiceeclipse.net:3478",
+      },
+      {
+        urls: "stun2.l.google.com:19302",
+      },
+      {
+        urls: "stun3.l.google.com:19302",
+      },
+      {
+        urls: "stun4.l.google.com:19302",
+      },
+      {
+        urls: "stunserver.org:3478",
+      },
+      {
+        urls: "relay.webwormhole.io",
+      },
+      {
         urls: "stun:stun.relay.metered.ca:80",
       },
       {
-        urls: "turn:br.relay.metered.ca:80",
-        username: "35060b797397c62c3a289143",
-        credential: "yPJEBHy/CjnHpT0i",
+        urls: "stun:stun.relay.metered.ca:80",
       },
       {
-        urls: "turn:br.relay.metered.ca:80?transport=tcp",
-        username: "35060b797397c62c3a289143",
-        credential: "yPJEBHy/CjnHpT0i",
-      },
-      {
-        urls: "turn:br.relay.metered.ca:443",
-        username: "35060b797397c62c3a289143",
-        credential: "yPJEBHy/CjnHpT0i",
-      },
-      {
-        urls: "turns:br.relay.metered.ca:443?transport=tcp",
-        username: "35060b797397c62c3a289143",
-        credential: "yPJEBHy/CjnHpT0i",
+        urls: "stun:stun.relay.metered.ca:80",
       }
     ]
   }
