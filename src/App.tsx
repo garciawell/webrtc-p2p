60f7a6be2945dff20969c34cc106b44498c8c977
Wellington Garcia <garciawell@gmail.com>
2024-11-07 17:56:40 -0300

    feat: turn server test

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,22 @@ const constraints = {
   video: true
 };
 
+
 const peer = new Peer({
   path: '/peerjs',
   host: 'concrete-kelly-garcia-2cd3d48b.koyeb.app',
-  // config: {
-  //   'iceServers': [
-  //     {
-  //       urls: "stun1.l.google.com:19302",
-  //     },
-  //     {
-  //       urls: "stun1.voiceeclipse.net:3478",
-  //     },
-  //     {
-  //       urls: "stun2.l.google.com:19302",
-  //     },
-  //     {
-  //       urls: "stun3.l.google.com:19302",
-  //     },
-  //     {
-  //       urls: "stun4.l.google.com:19302",
-  //     },
-  //     {
-  //       urls: "stunserver.org:3478",
-  //     },
-  //     {
-  //       urls: "relay.webwormhole.io",
-  //     },
-  //     {
-  //       urls: "stun:stun.relay.metered.ca:80",
-  //     },
-  //     {
-  //       urls: "stun:stun.relay.metered.ca:80",
-  //     },
-  //     {
-  //       urls: "stun:stun.relay.metered.ca:80",
-  //     }
-  //   ]
-  // }
+  config: {
+    'iceServers': [
+      {
+        urls: "stun:stun.l.google.com:19302",
+      },
+      {
+        urls: "turn:98.84.31.110:3478",
+        username: "garciawell",
+        credential: "qwer1234",
+      }
+    ]
+  }
 });
 
 
